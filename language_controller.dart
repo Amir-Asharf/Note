da226c406bddb6521316bcc56ba37a81dfa25445
Amir-Ashraf <154739352+Amir-Asharf@users.noreply.github.com>
2025-05-02 23:07:19 +0300

    Delete language_controller.dart

--- language_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class LanguageController extends GetxController {
-  void changelang(String lang) {
-    var locale = Locale(lang);
-    Get.updateLocale(locale);
-  }
-}
